5eafac4f1f1c765e11e9fbffccee6805c2ce826a
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-06 18:02:09 +0300

    WhiteList/BlackList IP control-1 Custom Middleware

---
+++ WhiteBlackList.Web/Middlewares/IPSafeMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace WhiteBlackList.Web.Middlewares
+{
+    public class IPSafeMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IPList _ipList;
+
+        public IPSafeMiddleware(RequestDelegate next, IOptions<IPList> ipListoptions)
+        {
+            _next = next;
+            _ipList = ipListoptions.Value;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            IPAddress requestIp = httpContext.Connection.RemoteIpAddress;
+            var isWhiteList = _ipList.WhiteList.Where(x => IPAddress.Parse(x).Equals(requestIp)).Any();
+
+            if (!isWhiteList)
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            await _next(httpContext);
+        }
+
+
+
+    }
+}
